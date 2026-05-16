86482d5e527559354abd9485fa61a287023d4c76
Aizz <sasit2555@gmail.com>
2026-05-16 12:12:52 +0700

    feat: integrate vue-gtag for Google Analytics tracking

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createGtag } from "vue-gtag";
 
-createApp(App).mount('#app')
+const gtag = createGtag({
+  tagId: "G-NCHP6M37W4",
+});
+
+createApp(App).use(gtag).mount("#app");
